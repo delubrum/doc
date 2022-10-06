fe4adcaa11de5208752528da0500ab0b4f11ccda
Felipe <lfelipecorreah@gmail.com>
2022-10-06 07:16:03 -0500

    no web

--- views/products/list.php
+++ views/products/list.php
@@ -10,12 +10,16 @@
             <th>Temporada</th>
             <th>Año</th>
             <th>Precio de Venta</th>
+            <th>Inventario</th>
+
             <!-- <th>Código de Barras</th> -->
             <th>Acciones</th>
         </tr>
     </thead>
     <tbody>
-        <?php foreach($this->products->list() as $r) { ?>
+        <?php foreach($this->products->list() as $r) { 
+        $qty = $this->purchases->getQty($r->id)->total - $this->sales->getQty($r->id)->total;
+        ?>
         <tr>
             <th><input style="width:50px" type="number" name="checkid" class="printqty" value="0" data-id="<?php echo $r->id ?>"></th>
             <td><?php echo $r->code ?></td>
@@ -26,6 +30,7 @@
             <td><?php echo  mb_convert_case($r->season, MB_CASE_TITLE, "UTF-8"); ?></td>
             <td><?php echo $r->year ?></td>
             <td><?php echo number_format($r->price,2) ?></td>
+            <td><?php echo $qty ?></td>
             <!-- <td><img src="middlewares/barcode.php?text='<?php echo $r->code ?>'&size=50&codetype=Code39&print=true" onclick="newWindow = window.open('middlewares/barcode.php?text=<?php echo $r->code ?>&size=50&codetype=Code39&print=true'); newWindow.print();"> </td> -->
             <td class="btn-group">
                 <div class="custom-control custom-switch pt-2">
